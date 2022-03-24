0cf395ca238604be99933005b68e822b5a9d691c
rkvishwa <99144322+vishwarkk@users.noreply.github.com>
2022-03-24 20:33:48 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_NAME','u872931566_vimeo');
 define('USER','u872931566_vishwa12');
-define('PW','0770499787kK#');
+define('PW','0770499787kK#1');
 
 if(!$con = mysqli_connect(DB_HOST,USER,PW,DB_NAME)){
 	die('Failed To connect');
