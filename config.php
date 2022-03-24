3b0825f075dd156ddc1affec6f7f0d53a9612ef5
rkvishwa <99144322+vishwarkk@users.noreply.github.com>
2022-03-24 20:34:47 +0530

    Update config.php

--- config.php
+++ config.php
@@ -6,5 +6,6 @@ define('USER','u872931566_vishwa12');
 define('PW','0770499787kK#1');
 
 if(!$con = mysqli_connect(DB_HOST,USER,PW,DB_NAME)){
-	die('Failed To connect');
+	die("Failed To connect");
 }
+?>
