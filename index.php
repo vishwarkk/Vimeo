41f83e6e3144e63a83a6c26b9f8c21b2d856ca51
rkvishwa <99144322+vishwarkk@users.noreply.github.com>
2022-03-25 10:59:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if(isset($_POST['protect'])){
 if(isset($_POST['whitelist'])){
 	$uri = getthis($con,'class1','uri','id',3);
 	$domain = $_POST['domain'];
-	$client->request($uri . "/privacy/domains/delegate.lk");
+	$client->request($uri . "/privacy/domains/delegate.lk", 'PUT');
 	$client->request($uri, array(
 	  'privacy' => array(
 	    'embed' => 'whitelist'
