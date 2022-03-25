51e5721db40bb8ff88c1be74eb7b8082c67eed66
rkvishwa <99144322+vishwarkk@users.noreply.github.com>
2022-03-25 11:18:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ if(isset($_POST['whitelist'])){
 	  'privacy' => array(
 	    'embed' => 'whitelist'
 	  )
-	), 'PATCH');
+	), 'PATCH','PUT');
 
 	//echo $uri . " will only be embeddable on http://".$domain;
 }
