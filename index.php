5ebd370a4ace3c526ad597aee411396bb03659d7
rkvishwa <99144322+vishwarkk@users.noreply.github.com>
2022-03-25 11:20:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -65,12 +65,13 @@ if(isset($_POST['protect'])){
 if(isset($_POST['whitelist'])){
 	$uri = getthis($con,'class1','uri','id',3);
 	$domain = $_POST['domain'];
-	$client->request($uri . "/privacy/domains/delegate.lk",'PUT');
+	$client->request($uri . "/privacy/domains/delegate.lk");
 	$client->request($uri, array(
 	  'privacy' => array(
 	    'embed' => 'whitelist'
 	  )
 	), 'PATCH');
+	$client->request('PUT');
 
 	//echo $uri . " will only be embeddable on http://".$domain;
 }
