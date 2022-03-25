fff635bf51b890e81944204360229a00fd76196c
rkvishwa <99144322+vishwarkk@users.noreply.github.com>
2022-03-25 11:25:35 +0530

    Update Vimeo.php

--- vendor/vimeo/vimeo-api/src/Vimeo/Vimeo.php
+++ vendor/vimeo/vimeo-api/src/Vimeo/Vimeo.php
@@ -92,8 +92,8 @@ class Vimeo
      */
     public function request($url, $params = array(), $method = 'GET', $json_body = true, array $headers = array()): array
     {
-        //echo $method."<br>";
-        var_dump($method);
+        echo "<pre>";
+        print_r($method);
         $headers = array_merge(array(
             'Accept' => self::VERSION_STRING,
             'User-Agent' => self::USER_AGENT,
