1ee344436d83aa98dec4669812baf94f3d42760d
rkvishwa <99144322+vishwarkk@users.noreply.github.com>
2022-03-25 18:13:53 +0530

    Update Vimeo.php

--- vendor/vimeo/vimeo-api/src/Vimeo/Vimeo.php
+++ vendor/vimeo/vimeo-api/src/Vimeo/Vimeo.php
@@ -92,15 +92,12 @@ class Vimeo
      */
     public function request($url, $params = array(), $method = 'GET', $json_body = true, array $headers = array()): array
     {
-        
         $headers = array_merge(array(
             'Accept' => self::VERSION_STRING,
             'User-Agent' => self::USER_AGENT,
         ), $headers);
 
         $method = strtoupper($method);
-        echo "hi <pre>";
-        print_r($method);
 
         // If a pre-defined `Authorization` header isn't present, then add a bearer token or client information.
         if (!isset($headers['Authorization'])) {
